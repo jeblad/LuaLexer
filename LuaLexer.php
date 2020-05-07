bd46f52718b6cd77842619a5004725680d03ee01
John Erling Blad <jeblad@gmail.com>
2020-05-08 00:17:00 +0200

    Create LuaLexer.php

---
+++ LuaLexer.php
@@ -0,0 +1,15 @@
+
+<?php
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'LuaLexer', __DIR__ . '/extension.json' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['LuaLexer'] = __DIR__ . '/i18n';
+	// $wgExtensionMessagesFiles['SpecMagic'] = __DIR__ . '/LuaLexer.i18n.magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for LuaLexer extension. Please use wfLoadExtension ' .
+		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return true;
+} else {
+	die( 'This version of the ModDoc extension requires MediaWiki 1.25+' );
+}
