b9c20bdf033df9df060eb4200d4fcf767e03d592
John Erling Blad <jeblad@gmail.com>
2020-05-08 12:11:47 +0200

    Modified LuaLexer.php

--- LuaLexer.php
+++ LuaLexer.php
@@ -11,5 +11,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	);
 	return true;
 } else {
-	die( 'This version of the ModDoc extension requires MediaWiki 1.25+' );
+	die( 'This version of the LuaLexer extension requires MediaWiki 1.25+' );
 }
